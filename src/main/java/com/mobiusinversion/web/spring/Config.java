c42a868087836cd9ab98e00d66322b875fecdff4
David Williams <mobiusinversion@gmail.com>
2014-03-22 08:38:59 -0700

    Java config

---
+++ src/main/java/com/mobiusinversion/web/spring/Config.java
@@ -0,0 +1,24 @@
+package com.mobiusinversion.web.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan("com.mobiusinversion.web")
+public class Config {
+
+    @Bean
+    public UrlBasedViewResolver setupViewResolver() {
+        UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+        resolver.setPrefix("/WEB-INF/jsp/");
+        resolver.setSuffix(".jsp");
+        resolver.setViewClass(JstlView.class);
+        return resolver;
+    }
+
+}
